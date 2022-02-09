c96f73d46dc936e006a7d1bab7d149f488f34ffa
Sean Hammell <sean.m.hammell@gmail.com>
2022-02-09 13:21:43 -0500

    Add functions for each operation and 'operate' function to call them

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operation, a, b) {
+  switch (operation) {
+    case 'addition':
+      return add(a, b);
+    case 'subtraction':
+      return subtract(a, b);
+    case 'multiplication':
+      return multiply(a, b);
+    case 'division':
+      return divide(a, b);
+  }
+}
+
 function createKeyboard() {
   const keyboard = document.querySelector('.keyboard');
 
