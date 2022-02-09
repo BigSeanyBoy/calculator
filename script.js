1ee395fad205b1155d2e6602d88fb3fb1a122b8a
Sean Hammell <sean.m.hammell@gmail.com>
2022-02-09 13:14:13 -0500

    Add operator keys and createKeyboard function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function main() {
+function createKeyboard() {
   const keyboard = document.querySelector('.keyboard');
 
   for (let i = 0; i < 10; i++) {
@@ -9,6 +9,43 @@ function main() {
 
     keyboard.appendChild(key);
   }
+
+  const operators = [
+    {
+      name: 'addition',
+      symbol: '+',
+    },
+    {
+      name: 'subtraction',
+      symbol: '-',
+    },
+    {
+      name: 'multiplication',
+      symbol: '*',
+    },
+    {
+      name: 'division',
+      symbol: '/',
+    },
+    {
+      name: 'evaluate',
+      symbol: '=',
+    },
+  ]
+
+  operators.forEach(operator => {
+    const key = document.createElement('button');
+    key.classList.add('key');
+    key.classList.add('operator');
+    key.setAttribute('id', operator.name);
+    key.textContent = operator.symbol;
+
+    keyboard.appendChild(key);
+  });
+}
+
+function main() {
+  createKeyboard();
 }
 
 main();
