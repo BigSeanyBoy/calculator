25565f8500ef162c31af7832cef68ad8e2a033c8
Sean Hammell <sean.m.hammell@gmail.com>
2022-02-10 14:12:25 -0500

    Update button layout

--- script.js
+++ script.js
@@ -87,65 +87,88 @@ function populateDisplay(str) {
 function createKeyboard() {
   const keyboard = document.querySelector('.keyboard');
 
-  for (let i = 0; i < 10; i++) {
-    const key = document.createElement('button');
-    key.classList.add('key');
-    key.setAttribute('id', `${i}`);
-    key.textContent = `${i}`;
-    key.addEventListener('click', () => {
-      populateDisplay(key.textContent);
-    });
-
-    keyboard.appendChild(key);
-  }
-
   const operators = [
-    {
-      name: 'addition',
-      symbol: '+',
-    },
-    {
-      name: 'subtraction',
-      symbol: '-',
-    },
-    {
-      name: 'multiplication',
-      symbol: '*',
-    },
-    {
-      name: 'division',
-      symbol: '/',
-    },
-    {
-      name: 'evaluate',
-      symbol: '=',
-    },
-    {
-      name: 'clear',
-      symbol: 'AC'
-    },
+    {name: 'addition', symbol: '+'},
+    {name: 'subtraction', symbol: '-'},
+    {name: 'multiplication', symbol: '*'},
+    {name: 'division', symbol: '/',},
   ];
 
-  operators.forEach(operator => {
+  for (let i = 2; i >= 0; i--) {
+    const row = document.createElement('div');
+    for (let j = 1; j < 4; j++) {
+      const currentNum = 3 * i + j;
+      const key = document.createElement('button');
+      key.classList.add('key');
+      key.setAttribute('id', `${currentNum}`);
+      key.textContent = `${currentNum}`;
+      key.addEventListener('click', () => {
+        populateDisplay(key.textContent);
+      });
+
+      row.appendChild(key);
+    }
     const key = document.createElement('button');
     key.classList.add('key');
     key.classList.add('operator');
-    key.setAttribute('id', operator.name);
-    key.textContent = operator.symbol;
-    if (operator.symbol == '=') {
-      key.addEventListener('click', () => {
-        const displayValue = document.querySelector('.output').textContent;
-        evaluate(displayValue);
-      });
-    }
-    else if (operator.symbol == 'AC') {
-      key.addEventListener('click', () => populateDisplay(""));
-    }
-    else {
-      key.addEventListener('click', () => populateDisplay(key.textContent));
-    }
-    keyboard.appendChild(key);
+    key.setAttribute('id', operators[i+1].name);
+    key.textContent = operators[i+1].symbol;
+    key.addEventListener('click', () => populateDisplay(key.textContent));
+    row.appendChild(key);
+    
+    keyboard.appendChild(row);
+  }
+
+  const bottomRow = document.createElement('div');
+
+  const zero = document.createElement('button');
+  zero.classList.add('key');
+  zero.setAttribute('id', '0');
+  zero.textContent = '0';
+  zero.addEventListener('click', () => {
+    populateDisplay(zero.textContent);
   });
+  bottomRow.appendChild(zero);
+
+  const decimal = document.createElement('button');
+  decimal.classList.add('key');
+  decimal.setAttribute('id', '.');
+  decimal.textContent = '.';
+  decimal.addEventListener('click', () => {
+    if (!document.querySelector('.output').textContent.split("").includes('.')) {
+      populateDisplay(decimal.textContent);
+    }
+  });
+  bottomRow.appendChild(decimal);
+
+  const equals = document.createElement('button');
+  equals.classList.add('key');
+  equals.classList.add('operator');
+  equals.setAttribute('id', 'evaluate');
+  equals.textContent = '=';
+  equals.addEventListener('click', () => {
+    const displayValue = document.querySelector('.output').textContent;
+    evaluate(displayValue);
+  });
+  bottomRow.appendChild(equals);
+
+  const addition = document.createElement('button');
+  addition.classList.add('key');
+  addition.classList.add('operator');
+  addition.setAttribute('id', 'addition');
+  addition.textContent = '+';
+  addition.addEventListener('click', () => populateDisplay(key.textContent));
+  bottomRow.appendChild(addition);
+
+  keyboard.appendChild(bottomRow);
+
+  const clear = document.createElement('button');
+  clear.classList.add('key');
+  clear.classList.add('operator');
+  clear.setAttribute('id', 'clear');
+  clear.textContent = 'AC';
+  clear.addEventListener('click', () => populateDisplay(""));
+  keyboard.appendChild(clear);
 }
 
 function main() {
